405e2734159a5193635349722a300fb8ca1482fd
emeequizve <ipom3993@gmail.com>
2026-07-25 22:07:31 -0400

    actualiza a v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'valor-hora-v1';
+const CACHE_NAME = 'valor-hora-v2';
 const urlsToCache = [
     './',
     './index.html',
@@ -7,16 +7,43 @@ const urlsToCache = [
     './manifest.json'
 ];
 
+// Instalar y cachear archivos
 self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => cache.addAll(urlsToCache))
     );
+    // Fuerza al nuevo SW a activarse inmediatamente
+    self.skipWaiting();
 });
 
+// Activar: borrar caches antiguas y tomar control
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames
+                    .filter(name => name !== CACHE_NAME)
+                    .map(name => caches.delete(name))
+            );
+        })
+    );
+    // Toma control de todas las pestañas abiertas
+    self.clients.claim();
+});
+
+// Fetch: primero red, luego caché (para siempre traer lo más nuevo)
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
+        fetch(event.request)
+            .then(networkResponse => {
+                return caches.open(CACHE_NAME).then(cache => {
+                    cache.put(event.request, networkResponse.clone());
+                    return networkResponse;
+                });
+            })
+            .catch(() => {
+                return caches.match(event.request);
+            })
     );
 });
